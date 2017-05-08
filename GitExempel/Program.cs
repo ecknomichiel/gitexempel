44fda034993734f1a8c5ef1a6a33d11e26bbbed2
tnordenmark <t.nordenmark@gmail.com>
2017-05-08 11:46:05 +0200

    test

--- GitExempel/Program.cs
+++ GitExempel/Program.cs
@@ -10,7 +10,7 @@ namespace GitExempel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); //Test
 	        Console.WriteLine("Hello World 2!"); // Testkommentar 2000
             Console.ReadKey();
         }

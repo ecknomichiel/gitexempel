86db40e053857afcf92c3d6e550ce36a4766caac
ecknomichiel <michiel@frostvikenit.eu>
2017-05-08 11:49:00 +0200

    min merge

--- GitExempel/Program.cs
+++ GitExempel/Program.cs
@@ -6,8 +6,8 @@ namespace GitExempel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-	        Console.WriteLine("Hello World 2!");
+            Console.WriteLine("Hello World!"); //comment rad 1
+	        Console.WriteLine("Hello World 2!"); //comment rad 2
             Console.ReadKey();
         }
     }
